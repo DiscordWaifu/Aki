b87c52b7f248e213370aaa710dad4d519acf1fe8
Kashall <17620516+Kashalls@users.noreply.github.com>
2022-08-20 18:28:03 -0700

    Correct intent bits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { GatewayIntentBits } = require('discord.js');
 const AkiClient = require('./library/AkiClient');
 
-const client = new AkiClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers] });
+const client = new AkiClient({ intents: [GatewayIntentBits.Guilds] });
 
 
 client.login(process.env.DISCORD_TOKEN);
